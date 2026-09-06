8a7503c4cf505d4cfbc8e580ebd332cd85bcbe01
Heros-Tempus <Heros_Tempus@yahoo.com>
2026-09-06 11:32:58 -0400

    Refactor attribution formatting in buildAttribution function; remove newline characters for cleaner output.

--- cmd/song-queue-manager/main.go
+++ cmd/song-queue-manager/main.go
@@ -442,17 +442,17 @@ func (s *Service) StopAndWipe(ctx context.Context) error {
 
 func buildAttribution(t Track) string {
 	var parts []string
-	parts = append(parts, fmt.Sprintf("%s - %s\n", t.Artist, t.Track))
+	parts = append(parts, fmt.Sprintf("%s - %s", t.Artist, t.Track))
 
 	if t.SourceMedia != "" {
-		parts = append(parts, fmt.Sprintf("Source media: (%s)\n", t.SourceMedia))
+		parts = append(parts, fmt.Sprintf("Source media: (%s)", t.SourceMedia))
 	}
 	if t.Album != "" {
-		parts = append(parts, fmt.Sprintf("Album: [%s]\n", t.Album))
+		parts = append(parts, fmt.Sprintf("Album: [%s]", t.Album))
 	}
 	if t.OriginalComposer != "" {
-		parts = append(parts, fmt.Sprintf("Composed by: %s\n", t.OriginalComposer))
+		parts = append(parts, fmt.Sprintf("Composed by: %s", t.OriginalComposer))
 	}
 
-	return strings.Join(parts, " ")
+	return strings.Join(parts, "\n")
 }
